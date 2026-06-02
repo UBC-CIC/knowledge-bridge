81531ef028599bc4eecd1b023c1e62ec1305c194
loganihrishi <loganihrishi@gmail.com>
2026-06-02 15:18:12 -0700

    update frontend

--- cdk/lib/api-stack.ts
+++ cdk/lib/api-stack.ts
@@ -288,8 +288,8 @@ export class ApiGatewayStack extends cdk.Stack {
           cognito.OAuthScope.EMAIL,
           cognito.OAuthScope.PROFILE,
         ],
-        callbackUrls: [amplifyCallbackUrl, "http://localhost:5173"],
-        logoutUrls: [amplifyCallbackUrl, "http://localhost:5173"],
+        callbackUrls: [amplifyCallbackUrl, `${amplifyCallbackUrl}/landing`, "http://localhost:5173", "http://localhost:5173/landing"],
+        logoutUrls: [amplifyCallbackUrl, `${amplifyCallbackUrl}/landing`, "http://localhost:5173", "http://localhost:5173/landing"],
       },
     });
     this.appClient.node.addDependency(entraOidcProvider);
